74878a1c306711ad84b63accd345b51694c32fb3
Brent Rodriguez <brent.s.rodriguez@gmail.com>
2021-01-09 21:11:10 -0600

    Start Condition

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const score0_El = document.getElementById('score--0')
+const score1_El = document.getElementById('score--1')
+const dice_El = document.querySelector('.dice')
+
+
+//Starting Conditions
+score0_El.textContent = 0
+score1_El.textContent = 0
+dice_El.classList.add('hidden')
