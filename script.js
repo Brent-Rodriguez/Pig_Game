d9814eb165643b36d2dece539d2fcd5bd47501ab
Brent Rodriguez <brent.s.rodriguez@gmail.com>
2021-01-10 01:03:48 -0600

    Refactor Code

--- script.js
+++ script.js
@@ -6,13 +6,10 @@ const score0_El = document.getElementById('score--0')
 const score1_El = document.getElementById('score--1')
 const current0_El = document.getElementById('current--0')
 const current1_El = document.getElementById('current--1')
-
-
 const dice_El = document.querySelector('.dice')
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
-
 
 let scores, 
     currentScore, 
@@ -20,7 +17,7 @@ let scores,
     playing
 
 const init = () => {
-  //Starting Conditions
+  //Initializing Conditions
   scores = [0, 0]
   currentScore = 0
   activePlayer = 0
@@ -38,9 +35,10 @@ const init = () => {
   player1_El.classList.remove('player--active')
 }
 
+//Initialize Condition
 init()
 
-
+//Switch Player Function
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0
     activePlayer = activePlayer === 0 ? 1 : 0
@@ -49,7 +47,7 @@ const switchPlayer = () => {
     player1_El.classList.toggle('player--active')
 }
 
-//Roll Dice
+//Roll Dice Event
 btnRoll.addEventListener('click', () => {
   if(playing){
     const dice = Math.trunc(Math.random() * 6) + 1   //Random Dice Roll
@@ -66,7 +64,7 @@ btnRoll.addEventListener('click', () => {
   
 })
 
-//Hold Current Score
+//Hold Current Score Event
 btnHold.addEventListener('click', () => {
   if(playing) {
     scores[activePlayer] += currentScore   //Add current scors to Active Player
@@ -84,5 +82,5 @@ btnHold.addEventListener('click', () => {
   }
 })
   
-//New game
+//New game Event
 btnNew.addEventListener('click', init)
